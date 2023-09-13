18456dbfee698e436667b819ae7e2dbf84f6ac8e
HyeonTee <gusxo1820@icloud.com>
2023-09-13 23:10:53 +0900

    parse_uri

--- tiny/tiny.c
+++ tiny/tiny.c
@@ -102,6 +102,33 @@ void read_requesthdrs (rio_t *rp)
 
 int parse_uri (char *uri, char *filename, char *cgiargs)
 {
+	char *ptr;
+	
+	if (!strstr(uri, "cgi-bin")) {
+		/* Static content */
+		strcpy(cgiargs, "");
+		strcpy(filename, ".");
+		strcat(filename, uri);
+		if (uri[strlen(uri) - 1] == '/') {
+			strcat(filename, "home.html");
+		}
+		return 1;
+	} else {
+		/* Dynamic content */
+		ptr = index(uri, '?');
+		
+		if (ptr) {
+			strcpy(cgiargs, ptr + 1);
+			*ptr = '\0';
+		} else {
+			strcpy(cgiargs, "");
+		}
+		
+		strcpy(filename, ".");
+		strcat(filename, uri);
+		return 0;
+	}
+	
 }
 
 void serve_static (int fd, char *filename, int filesize)
